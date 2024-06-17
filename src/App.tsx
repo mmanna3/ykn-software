5d25ff67d0750dd4cc747fd857072f8d99a91c88
mmanna3 <mmmannna3@gmail.com>
2024-06-17 18:14:24 -0300

    Instala eslint y prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './App.css';
 
 function App() {
-  
-
-  return (
-    <>
-    <h1 className="text-3xl font-bold underline">
-      YKN Software
-    </h1>
-    </>
-  )
+    return (
+        <>
+            <h1 className="text-3xl font-bold underline">YKN Software</h1>
+        </>
+    );
 }
 
-export default App
+export default App;
