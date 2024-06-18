acfe127349523977b8d28f291207d9ed435c48ac
mmanna3 <mmmannna3@gmail.com>
2024-06-18 02:31:37 -0300

    navbar y favicon

--- src/navbar.tsx
+++ src/navbar.tsx
@@ -1,3 +1,10 @@
+import Icono from './assets/icono-60.png';
+
 export default function Navbar() {
-    return <div className="h-20 flex items-center justify-center bg-red-100">Navbar</div>;
+    return (
+        <div className="h-20 flex items-center justify-center">
+            <img width="36" height="36" src={Icono} />
+            <h1 className="text-2xl ml-2 font-semibold tracking-wide text-[#7E27A3]">YKN</h1>
+        </div>
+    );
 }
