17c4b80eec6330aefef0683036b7ebb73b389793
mmanna3 <mmmannna3@gmail.com>
2024-06-18 02:52:31 -0300

    footer

--- src/footer.tsx
+++ src/footer.tsx
@@ -1,3 +1,11 @@
+import Contacto from './assets/contacto.png';
+import Linkedin from './assets/linkedin.png';
+
 export default function Footer() {
-    return <div className="flex h-20 items-center justify-center bg-blue-100">Footer</div>;
+    return (
+        <div className="flex h-20 items-center justify-center ">
+            <img width="36" height="36" src={Linkedin} className="mr-2" />
+            <img width="32" height="32" src={Contacto} className="ml-2" />
+        </div>
+    );
 }
--- src/seccion-principal.tsx
+++ src/seccion-principal.tsx
@@ -1,7 +1,3 @@
 export default function SeccionPrincipal() {
-    return (
-        <div className="h-[100%] items-center flex justify-center bg-yellow-100">
-            Sección principal
-        </div>
-    );
+    return <div className="h-[100%] items-center flex justify-center "></div>;
 }
