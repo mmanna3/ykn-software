c16172dc06adb52837ba0feb37e21a37ec34d762
mmanna3 <mmmannna3@gmail.com>
2024-06-19 14:51:09 -0300

    Agrega roboto

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,10 @@
 export default {
     content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
     theme: {
-        extend: {},
+        fontFamily: {
+            roboto: ['Roboto', 'sans-serif'],
+        },
+        extend: { colors: {} },
     },
     plugins: [],
 };
