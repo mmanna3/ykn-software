2e0ee37302726ead28e26d5905e45de2165e8110
mmanna3 <mmmannna3@gmail.com>
2024-06-21 02:16:19 -0300

    arregla email

--- src/footer.tsx
+++ src/footer.tsx
@@ -5,7 +5,7 @@ export default function Footer() {
     return (
         <div className="flex h-20 items-center justify-center">
             <img width="36" height="36" src={Linkedin} className="mr-2" />
-            <a href="mailto:ykn.software@gmail.com">
+            <a href="mailto:ykn.software.com@gmail.com">
                 <img width="32" height="32" src={Contacto} className="ml-2" />
             </a>
         </div>
