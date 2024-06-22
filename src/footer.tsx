8ecc95fb757ef57ac417ff9e8fd296ebe6a59fda
mmanna3 <mmmannna3@gmail.com>
2024-06-22 18:57:56 -0300

    Agrega link a linkedin

--- src/footer.tsx
+++ src/footer.tsx
@@ -4,7 +4,9 @@ import Linkedin from './assets/linkedin.png';
 export default function Footer() {
     return (
         <div className="flex h-20 items-center justify-center">
-            <img width="36" height="36" src={Linkedin} className="mr-2" />
+            <a href="https://www.linkedin.com/company/ykn-software">
+                <img width="36" height="36" src={Linkedin} className="mr-2" />
+            </a>
             <a href="mailto:ykn.software.com@gmail.com">
                 <img width="32" height="32" src={Contacto} className="ml-2" />
             </a>
